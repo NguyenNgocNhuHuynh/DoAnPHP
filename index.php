ff1588503ca85fb03d139fe1c5dfef6973ea43cb
0306181032@caothang.edu.vn <0306181032@caothang.edu.vn>
2020-09-24 21:42:37 +0700

    Tach file include

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include ("include/top.php");
+    include ("include/hearder.php");
+    include ("include/menu.php");
+    include ("include/silde.php");
+    include ("include/main.php");
+    include ("include/footer.php");
+    include ("include/end.php");
+?>
